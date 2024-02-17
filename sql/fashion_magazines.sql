9026e9d9b045cb678064e1255ff1270fcd8ce232
Barbi Adkins <badkins0043@gmail.com>
2024-02-17 15:53:34 -0500

    fixed missing info

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,8 +1,10 @@
-SELECT customers.customer_name AS "Customer"
-    PRINTF("$%.2f",SUM(subscriptions.price_per_month * subscriptions.subscription_length)) 
-FROM customers
-JOIN orders
+SELECT customers.customer_name AS 'Customer',
+    PRINTF("$%.2f", SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS 'Amount Due'
+FROM orders
+JOIN customers
+    ON orders.customer_id = customers.customer_id
+JOIN subscriptions
     ON orders.subscription_ID = subscriptions.subscription_id
 WHERE Orders.order_status = "unpaid"
-    AND Subscriptions.description = 'Fashion Magazine'
+    AND subscriptions.description = 'Fashion Magazine'
     ORDER BY Customer;
