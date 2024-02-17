a99deac229a47ad66fbcd5a290ee089672ffa185
Barbi Adkins <badkins0043@gmail.com>
2024-02-17 10:05:00 -0500

    adding programming

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,8 @@
--- Add your sql here
+SELECT customers.customer_name AS "Customer"
+    PRINTF("$%.2f",SUM(subscriptions.price_per_month * subscriptions.subscription_length)) 
+FROM customers
+JOIN orders
+    ON orders.subscription_ID = subscriptions.subscription_id
+WHERE Orders.order_status = "unpaid"
+    AND Subscriptions.description = 'Fashion Magazine'
+    ORDER BY Customer;
